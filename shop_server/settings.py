5f0a1d5046184d9ec7d2f585632107aa47e14751
Matvey Molchanov <mattaudia6@gmail.com>
2022-06-03 22:35:07 +0300

    load static

--- shop_server/settings.py
+++ shop_server/settings.py
@@ -113,14 +113,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 import os
 
-
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
